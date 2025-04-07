8a1d6c54206033f112da6636503d0474f141c210
Luke <ralfeyd@gmail.com>
2025-04-06 22:37:31 -0500

    Firstchange

--- Project 8.py
+++ Project 8.py
@@ -1,1 +1,23 @@
+# Luke Duran
+# 04/06/25
+
+import sqlite3
+
+conn = sqlite3.connect('customers.db')  #Creates the file
+cursor = conn.cursor()  # connects the file
+
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS customers (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        birthday TEXT,
+        email TEXT,
+        phone TEXT,
+        address TEXT,
+        contact_method TEXT
+    )
+''')
+
+conn.commit() # Save changes
+conn.close() # Close connection
 
